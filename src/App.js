6abe582c8b0d292e21f9d3aee62dc8d983abe7ed
Oscar Garcia <ogarcia@uwm.edu>
2020-07-31 09:28:24 -0500

    Created the structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-class app extends React.Component {
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './app.module.css';
+
+class App extends React.Component {
     render(){
         return (
-            <div>
-                <h1>app </h1>
+            <div className={styles.container}>
+                <Cards />
+                <h1> hiWWWWW</h1>
+                <CountryPicker />
+                <Chart />
             </div>
         )
     }
 }
+
+export default App;
